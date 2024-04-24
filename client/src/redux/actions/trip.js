69c31372030c5fa45d137b2252338da53893f923
Mustafa Kanat <mkanat6620@gmail.com>
2024-04-25 02:48:03 +0300

    d

--- client/src/redux/actions/trip.js
+++ client/src/redux/actions/trip.js
@@ -23,7 +23,7 @@ export const getTripsAction =
 
 export const getTripsAllAction = () => async (dispatch) => {
   try {
-    const response = await axios.get(`${BASE_URL}/trip/getAll`);
+    const response = await axios.get(`${BASE_URL}/trip`);
     localStorage.setItem("trips", JSON.stringify(response.data));
 
     console.log(response);
